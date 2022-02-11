eb05b14979c2fe1ba313a231dc4f988e10056c26
BrunaCaldas <98419748+BrunaCaldas@users.noreply.github.com>
2022-02-11 16:48:32 -0300

    11-02 22

--- sketch.js
+++ sketch.js
@@ -1,36 +1,37 @@
 var trex, trexCorrendo;
 var borda;
 var solo,chao;
+var vridro;
+
 function preload(){
-  trexCorrendo = loadAnimation("trex1.png","trex3.png","trex4.png");
-chao = loadImage("ground2.png");
+ trexCorrendo = loadAnimation("trex1.png","trex3.png","trex4.png");
+ chao = loadImage("ground2.png");
 }
 
 function setup(){
-  createCanvas(600,200)
-  trex = createSprite(50,160,20,50);
-  trex.addAnimation("correndo",trexCorrendo);
-  trex.scale = 0.5;
-  borda = createEdgeSprites();
-  solo = createSprite (200,180,400,20);
-  solo.addImage("chão",chao);
-  solo.x = solo.width/2;
+ createCanvas(600,200)
+ trex = createSprite(50,160,20,50);
+ trex.addAnimation("correndo",trexCorrendo);
+ trex.scale = 0.5;
+ borda = createEdgeSprites();
+ solo = createSprite (200,180,400,20);
+ solo.addImage("chão",chao);
+ solo.x = solo.width/2;
+ vridro = createSprite (200,190,400,10);
+ vridro.visible = false;
 }
 
 function draw(){
-  background(190);
-solo.velocityX = -2;
-if (solo.x <0){
-solo.x = solo.width/2;
+ background(190);
+ solo.velocityX = -2;
+ if (solo.x <0){
+ solo.x = solo.width/2;
 }
 
-  if(keyDown("space")){
-    trex.velocityY = -10;
-  }
-
-  trex.velocityY = trex.velocityY + 1;
-
-  trex.collide(solo);
-  
-  drawSprites();
+ if(keyDown("space")&&trex.y>=150){
+ trex.velocityY = -10;
+}
+ trex.velocityY = trex.velocityY + 1;
+ trex.collide(vridro);
+ drawSprites();
 }
