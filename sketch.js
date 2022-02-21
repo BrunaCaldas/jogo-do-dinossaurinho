7c3a0614bd26cbfcfbe0b3b1189f9a03b7e0e49a
BrunaCaldas <98419748+BrunaCaldas@users.noreply.github.com>
2022-02-21 17:56:44 -0300

    21-02 22

--- sketch.js
+++ sketch.js
@@ -2,23 +2,29 @@ var trex, trexCorrendo;
 var borda;
 var solo,chao;
 var vridro;
+var claudinei;
+var argodao;
+
 
 function preload(){
  trexCorrendo = loadAnimation("trex1.png","trex3.png","trex4.png");
  chao = loadImage("ground2.png");
+ argodao = loadImage ("cloud.png");
 }
 
 function setup(){
- createCanvas(600,200)
+ createCanvas(600,200);
+ solo = createSprite (200,180,400,20);
+ solo.addImage("chão",chao);
+ solo.x = solo.width/2;
  trex = createSprite(50,160,20,50);
  trex.addAnimation("correndo",trexCorrendo);
  trex.scale = 0.5;
  borda = createEdgeSprites();
- solo = createSprite (200,180,400,20);
- solo.addImage("chão",chao);
- solo.x = solo.width/2;
  vridro = createSprite (200,190,400,10);
  vridro.visible = false;
+ //var aleatorio = Math.round(random(1,100));
+ //console.log(aleatorio);
 }
 
 function draw(){
@@ -33,5 +39,21 @@ function draw(){
 }
  trex.velocityY = trex.velocityY + 1;
  trex.collide(vridro);
+ nuvens();
  drawSprites();
+}
+
+function nuvens(){
+if (frameCount%60===0){
+claudinei = createSprite (600, 100, 40, 10);
+claudinei.addImage (argodao);
+claudinei.scale = random(0.5,1);
+claudinei.y = Math.round(random(1,100));
+claudinei.velocityX = -3;
+claudinei.depth = trex.depth;
+trex.depth +=1;
+
+}
+
+
 }
